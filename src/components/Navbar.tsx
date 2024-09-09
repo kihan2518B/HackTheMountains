bee78c17882986118479d1c3baa2ad70cfc9f289
kihan2518B <kp648027@gmail.com>
2024-09-09 22:22:34 +0530

    Navbar and Notification bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,226 @@
+"use client";
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Link from 'next/link';
+import Badge from '@mui/material/Badge';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+import NotificationPanel from './NotificationPanel';
+
+const pages = [
+    { title: 'Home', link: '/', isCustomer: true, isProvider: true },
+    { title: 'Schedule', link: '/provider/schedule', isCustomer: false, isProvider: true },
+    { title: 'Dashboard', link: 'dashboard', isCustomer: false, isProvider: true },
+    { title: 'Become A Provider', link: '/provider/register', isCustomer: true, isProvider: false },
+];
+
+const settings = [
+    { title: 'Profile', link: 'profile', isCustomer: true, isProvider: true },
+    { title: 'Logout', link: '/logout', isCustomer: true, isProvider: true },
+];
+
+
+const Navbar = () => {
+    const router = useRouter();
+    const [notifications, unreadCount] = [[{
+        id: "01",
+        message: "Nothing vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv",
+        isRead: false
+    }, {
+        id: "02",
+        message: "Nothing vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv",
+        isRead: false
+    },], 2];
+    const [isProvider, setIsProvider] = useState<boolean | null>(null);
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+    const [anchorElNotifications, setAnchorElNotifications] = useState<null | HTMLElement>(null);
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("User") ?? "{}");
+        setIsProvider(user.isProvider ?? false);
+    }, []);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleOpenNotifications = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNotifications(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleCloseNotifications = () => {
+        setAnchorElNotifications(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        ReserveME
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.filter(page => page.isCustomer !== isProvider || page.isProvider === isProvider).map((page) => (
+                                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                                    <Link href={page.link}>
+                                        <Typography textAlign="center">{page.title}</Typography>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.filter(page => page.isCustomer !== isProvider || page.isProvider === isProvider).map((page) => (
+                            <Link key={page.title} href={page.link}>
+                                <Button
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page.title}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
+                        <Tooltip title="Notifications">
+                            <IconButton onClick={handleOpenNotifications} sx={{ p: 2 }}>
+                                <Badge badgeContent={unreadCount} color="error">
+                                    <NotificationsNoneIcon sx={{ color: 'white' }} />
+                                </Badge>
+                            </IconButton>
+                        </Tooltip>
+                        <NotificationPanel
+                            notifications={notifications}
+                            unreadCount={unreadCount}
+                            handleClose={handleCloseNotifications}
+                            anchorElNotifications={anchorElNotifications}
+                        />
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.filter(setting => setting.isCustomer !== isProvider || setting.isProvider === isProvider).map((setting) => (
+                                <MenuItem key={setting.title} onClick={handleCloseUserMenu}>
+                                    <Link href={setting.link}>
+                                        <Typography textAlign="center">{setting.title}</Typography>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Navbar;
